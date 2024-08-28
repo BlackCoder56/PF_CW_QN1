7733649b7ecfec7f6a8be34720d23d04e2919fe9
BlackCoder56 <blackcoda56@gmail.com>
2024-08-28 09:08:48 +0300

    last commit

--- main.cpp
+++ main.cpp
@@ -2,15 +2,43 @@
 #include <string>
 using namespace std;
 
-void banking_service(){
-    double account_bal = 0;
-    cout << "\nSelect banking service: ";
-    cout <<"\n-> Deposit Money";
-    cout <<"\n-> Withdraw Money";
-    cout <<"\n-> Request Balance";
-    cout <<"\n-> Quit";
-    cout <<"\n > ";
-    cin >> account_bal;
+void banking_service()
+{
+       double account_balance = 0.0;
+       double withdraw_amount;
+       double deposit_amount;
+
+       // options
+       char opt;
+       char x = 'y';
+
+       while(x){
+            cout << "\nSelect banking options below:\n";
+            cout << "d -> Deposit Money.\n";
+            cout << "w -> Withdraw Money\n";
+            cout << "r -> Request Balance\n";
+            cout << "q -> Quit\n";
+            cout << "   >";
+            cin >> opt;
+
+            if(opt == 'w'){
+                 cout << "Enter amount to withdraw: $";
+                 cin >> withdraw_amount;
+                 account_balance -= withdraw_amount;
+            }else if(opt == 'd'){
+                cout << "Enter amount to deposit: $";
+                cin >> deposit_amount;
+                account_balance += deposit_amount;
+            }else if(opt == 'r'){
+                cout << "Your balance is $"<<account_balance;
+            }else if(opt == 'q'){
+                break;
+            }
+            else{
+                cout<<"Enter valid option(ie.d,w,r,q)";
+            }
+
+        }
 }
 
 
@@ -27,9 +55,9 @@ int main()
 
     while(isloggedin == false && want == true){
         cout << "\nPlease select an option from the menu below\n";
-        cout << "l-> Login\n";
-        cout << "c-> Create New Account\n";
-        cout << "q-> Quit\n> ";
+        cout << "l -> Login\n";
+        cout << "c -> Create New Account\n";
+        cout << "q -> Quit\n  > ";
         cin >> option;
 
         switch(option){
@@ -63,13 +91,13 @@ int main()
             }
 
         case 'q':
-            cout << "Thank You for using our services.\n";
+            cout << "\nThank You for using our services.\n";
             want = false;
             break;
         default:
-            cout << "Please enter valid options(eg.l,c,q)\n";
-        }
+            cout << "\nPlease enter valid options(eg.l,c,q)\n";
 
+        }
 
     }
 
